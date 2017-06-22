d64b6e50b768c50201602c39da1e3a8505bea9ff
fevral <februaryelegy@openmailbox.org>
2017-06-21 22:46:04 -0400

    Updated antiSolve addresses for the properly unpacked binary

--- angr/blog/AntiD/antiSolve.py
+++ angr/blog/AntiD/antiSolve.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python
 import angr
 
-b = angr.Project("AntiD_clean.exe", load_options={"auto_load_libs":False})
-s = b.factory.blank_state(addr=0x11411B0)
+b = angr.Project("AntiD_unpacked.exe", load_options={"auto_load_libs":False})
+s = b.factory.blank_state(addr=0x4011B0)
 
 s.mem[s.regs.esp+4:].dword = 0
 
 s.memory.store(0, s.se.BVS("ans", 0x28*8))
 
 pg = b.factory.path_group(s)
-pg.explore(find=0x114135D)
+pg.explore(find=0x40135D)
 for found in pg.found:
     print found.state.se.any_str(found.state.memory.load(0, 0x28)).strip('\0')
